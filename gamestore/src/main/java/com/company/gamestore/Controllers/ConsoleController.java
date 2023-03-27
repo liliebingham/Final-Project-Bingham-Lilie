17376c8e61fb4726d9eb157a88fa5fb6cf1ce6c3
liliebingham <69400913+liliebingham@users.noreply.github.com>
2023-03-27 16:04:36 -0700

    did controller and repo

--- gamestore/src/main/java/com/company/gamestore/Controllers/ConsoleController.java
+++ gamestore/src/main/java/com/company/gamestore/Controllers/ConsoleController.java
@@ -1,4 +1,67 @@
 package com.company.gamestore.Controllers;
 
+import com.company.gamestore.Models.Console;
+import com.company.gamestore.Repositories.ConsoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
 public class ConsoleController {
+
+    @Autowired
+    ConsoleRepository repo;
+
+
+    // POST route that creates a console
+    @PostMapping("/consoles")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Console addConsole(@RequestBody Console console) {
+        return repo.save(console);
+    }
+
+    // GET route that reads console by ID
+    @GetMapping("/consoles/{id}")
+    public Console getConsoleById(@PathVariable Integer id) {
+        Optional<Console> returnVal = repo.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
+    }
+
+
+
+    // GET route that gets all consoles
+    @GetMapping("/consoles")
+    public List<Console> getConsoles() {
+        return repo.findAll();
+    }
+
+    // PUT route that updates a console
+    @PutMapping("/authors")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateConsole(@RequestBody Console console) {
+        repo.save(console);
+    }
+
+    // DELETE route that deletes a console
+    @DeleteMapping("/consoles/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteConsole(@PathVariable int id) {
+        repo.deleteById(id);
+    }
+
+    //A GET route that returns all consoles from a certain manufacturer
+    @GetMapping("/books/author/{authorId}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public List<Console> getConsoleByManufacturer(@PathVariable String manufacturer) {
+
+        return repo.findByManufacturer(manufacturer);
+    }
 }
--- gamestore/src/main/java/com/company/gamestore/Repositories/ConsoleRepository.java
+++ gamestore/src/main/java/com/company/gamestore/Repositories/ConsoleRepository.java
@@ -1,8 +1,15 @@
 package com.company.gamestore.Repositories;
 
-import java.math.BigDecimal;
+import com.company.gamestore.Models.Console;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
-public class ConsoleRepository {
+import java.util.List;
 
+
+@Component
+public interface ConsoleRepository  extends JpaRepository<Console, Integer> {
+
+    List<Console> findByManufacturer(String manufacturer);
 
 }
