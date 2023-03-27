75594a0e017aa9ddf60c7bbd0fa51d69b3151306
Evelyn Quintero Ochoa <evelynquintero@berkeley.edu>
2023-03-27 15:48:35 -0700

    Created Game Model

---
+++ gamestore/src/main/java/com/company/gamestore/Models/Game.java
@@ -0,0 +1,99 @@
+package com.company.gamestore.Models;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer game_id;
+    @NotNull
+    private String title;
+    @NotNull
+    private String esrb_rating;
+    @NotNull
+    private String description;
+    @NotNull
+    private BigDecimal price;
+    @NotNull
+    private String studio;
+    private Integer quantity;
+
+    public Integer getGame_id() {
+        return game_id;
+    }
+
+    public void setGame_id(Integer game_id) {
+        this.game_id = game_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getEsrb_rating() {
+        return esrb_rating;
+    }
+
+    public void setEsrb_rating(String esrb_rating) {
+        this.esrb_rating = esrb_rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Game)) return false;
+        Game game = (Game) o;
+        return Objects.equals(getGame_id(), game.getGame_id()) && Objects.equals(getTitle(), game.getTitle()) && Objects.equals(getEsrb_rating(), game.getEsrb_rating()) && Objects.equals(getDescription(), game.getDescription()) && Objects.equals(getPrice(), game.getPrice()) && Objects.equals(getStudio(), game.getStudio()) && Objects.equals(getQuantity(), game.getQuantity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getGame_id(), getTitle(), getEsrb_rating(), getDescription(), getPrice(), getStudio(), getQuantity());
+    }
+}
