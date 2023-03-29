3fdae7586531ee668465ae33e803ccc3458dab2a
liliebingham <69400913+liliebingham@users.noreply.github.com>
2023-03-29 15:55:51 -0700

    updated console

--- gamestore/src/main/java/com/company/gamestore/Repositories/ConsoleRepository.java
+++ gamestore/src/main/java/com/company/gamestore/Repositories/ConsoleRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 
 @Repository
-public interface ConsoleRepository  extends JpaRepository<Console, Integer> {
+public interface ConsoleRepository extends JpaRepository<Console, Integer> {
 
     List<Console> findByManufacturer(String manufacturer);
 
--- gamestore/src/test/java/com/company/gamestore/Service/ServiceLayerTest.java
+++
@@ -1,4 +0,0 @@
-package com.company.gamestore.Service;
-
-public class ServiceLayerTest {
-}
